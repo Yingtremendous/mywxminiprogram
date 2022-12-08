279643d7e59a6d8ce61959e8fbbf30bb8d9e97e4
Ying Gu <guying005@googlemail.com>
2022-12-08 12:09:50 -0300

    add tabbar for miniprogram

--- unpackage/dist/dev/mp-weixin/app.js
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports[Symbol.toStringTag] = "Module";
 var common_vendor = require("./common/vendor.js");
 if (!Math) {
-  "./pages/index/index.js";
+  "./pages/home/home.js";
+  "./pages/cate/cate.js";
+  "./pages/cart/cart.js";
+  "./pages/my/my.js";
 }
 const _sfc_main = {
   onLaunch: function() {
